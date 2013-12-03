47fe79c3772b83bc3430e9ab9d22a62a2af5a248
philipleaning <philipleaning@gmx.com>
2013-12-03 22:18:11 +0000

    Navigation reverses to line at belt/sleigh

--- software/robot/navigation.cpp
+++ software/robot/navigation.cpp
@@ -135,7 +135,11 @@ void navigate(){
             catch(...){
                 throw;
             }
-		
+			//If starting node is belt/sleigh, reverse robot onto line before continuing 
+			if( (status.current_node==4 || status.current_node==11 || status.current_node==10) && i=1){
+				reverse_to_line(turn);
+				continue;
+			}
 		
 		    cout << "Turning ";
 		    print_turn(turn);
